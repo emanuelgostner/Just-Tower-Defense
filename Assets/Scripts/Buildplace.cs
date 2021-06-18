03689be49158f47b70bcf62734c73f437b9d6a2b
Emanuel Gostner <emanuel.gostner@crystal-design.com>
2021-06-18 22:52:14 +0200

    add tower and tower menu

--- Assets/Scripts/Buildplace.cs
+++ Assets/Scripts/Buildplace.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Buildplace : MonoBehaviour {
     // Tower gameObject. Public variables can be assigned via GUI
     public GameObject towerPrefab;
+    public Button placeTower;
+    public Button upgradeTower;
+    public Button removeTower;
+    private int maxLevel = 2;
+    private int curLevel = 0;
+
 
     // Built in unity function. Triggered when click and release
     void OnMouseUpAsButton() {
         // Build Tower above Buildplace
     	GameObject g = (GameObject)Instantiate(towerPrefab);
     	g.transform.position = transform.position + Vector3.up;
+        if (curLevel == 0)
+        {
+            // place has no tower on it
+            placeTower.gameObject.SetActive(true);
+        }
     }
 }
