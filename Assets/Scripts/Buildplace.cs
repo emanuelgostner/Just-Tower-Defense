e22370bd6a6a167f38d679e7eb68c0beecb0de71
Emanuel Gostner <emanuel.gostner@crystal-design.com>
2021-06-26 23:40:49 +0200

    tower menu

--- Assets/Scripts/Buildplace.cs
+++ Assets/Scripts/Buildplace.cs
@@ -6,29 +6,53 @@ using UnityEngine.UI;
 
 public class Buildplace : MonoBehaviour {
     // Tower gameObject. Public variables can be assigned via GUI
-    public GameObject towerPrefab;
-    public Button placeTower;
-    public Button upgradeTower;
-    public Button removeTower;
+    private GameObject towerSettings;
+    public GameObject towerPrefab0;
+    public GameObject towerPrefab1;
+    public GameObject towerPrefab2;
     private int maxLevel = 2;
     private int curLevel = 0;
 
-
+    void Start()
+    {
+        towerSettings = GameObject.Find("TowerSettings");
+    }
     // Built in unity function. Triggered when click and release
     void OnMouseUpAsButton() {
+        towerSettings.GetComponent<SelectedTower>().SetSelectedBuildplace(this.gameObject);
+        Debug.Log("Buildplace clicked", this);
         // Build Tower above Buildplace
-    	GameObject g = (GameObject)Instantiate(towerPrefab);
+        /*GameObject g = (GameObject)Instantiate(towerPrefab0);
     	g.transform.position = transform.position + Vector3.up;
         if (curLevel == 0)
         {
             // place has no tower on it
-            placeTower.gameObject.SetActive(true);
-        }
+            //placeTower0.gameObject.SetActive(true);
+        }*/
     }
 
-    void upgradeTowerMe()
+    public void placeTower()
     {
-        CoinBalance coinBalance = CoinBalance.Instance;
+        //CoinBalance coinBalance = CoinBalance.Instance;
+        Debug.Log("place Tower", this);
     }
-    
+    public void upgradeTower()
+    {
+        //CoinBalance coinBalance = CoinBalance.Instance;
+        Debug.Log("upgrade Tower", this);
+    }
+    public void removeTower()
+    {
+        //CoinBalance coinBalance = CoinBalance.Instance;
+        Debug.Log("remove Tower", this);
+    }
+
+    public int getCurLevel()
+    {
+        return this.curLevel;
+    }
+    public int getMaxLevel()
+    {
+        return this.maxLevel;
+    }
 }
---
+++ Assets/Scripts/SelectedTower.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectedTower : MonoBehaviour
+{
+    private GameObject selectedBuildplace;
+    public Button closeMenuObj;
+    public Button placeTowerObj;
+    public Button upgradeTowerObj;
+    public Button removeTowerObj;
+    
+    private void handleButtonVisibility()
+    {
+        // No buildable place selected
+        if (!selectedBuildplace)
+        {
+            closeMenuObj.gameObject.SetActive(false);
+            placeTowerObj.gameObject.SetActive(false);
+            upgradeTowerObj.gameObject.SetActive(false);
+            removeTowerObj.gameObject.SetActive(false);
+        }
+        // Buildable place has no building yet
+        else if (selectedBuildplace.GetComponent<Buildplace>().getCurLevel() == 0)
+        {
+            closeMenuObj.gameObject.SetActive(true);
+            placeTowerObj.gameObject.SetActive(true);
+        }
+        // Buildable place has a upgradable tower 
+        else if (selectedBuildplace.GetComponent<Buildplace>().getCurLevel() >
+                 selectedBuildplace.GetComponent<Buildplace>().getMaxLevel())
+        {
+            closeMenuObj.gameObject.SetActive(true);
+            upgradeTowerObj.gameObject.SetActive(false);
+            removeTowerObj.gameObject.SetActive(false);
+        }
+        // Builable place has tower with max level
+        else
+        {
+            removeTowerObj.gameObject.SetActive(false);
+        }
+    }
+    public void SetSelectedBuildplace(GameObject buildplace)
+    {
+        this.selectedBuildplace = buildplace;
+        Debug.Log("Buildplace updated", selectedBuildplace);
+        handleButtonVisibility();
+    }
+
+    public void placeTower()
+    {
+        selectedBuildplace.GetComponent<Buildplace>().placeTower();
+    }
+
+    public void upgradeTower()
+    {
+        selectedBuildplace.GetComponent<Buildplace>().upgradeTower();
+    }
+
+    public void removeTower()
+    {
+        selectedBuildplace.GetComponent<Buildplace>().removeTower();
+    }
+
+    public void closeMenu()
+    {
+        selectedBuildplace = null;
+        handleButtonVisibility();
+    }
+}
