5f6ea03bd03b0daa4c3126e8f1112f95161971b9
Emanuel Gostner <emanuel.gostner@crystal-design.com>
2021-06-26 21:09:38 +0200

    dev

--- Assets/Scripts/Buildplace.cs
+++ Assets/Scripts/Buildplace.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Buildplace : MonoBehaviour {
     // Tower gameObject. Public variables can be assigned via GUI
     public GameObject towerPrefab;
@@ -24,4 +25,10 @@ public class Buildplace : MonoBehaviour {
             placeTower.gameObject.SetActive(true);
         }
     }
+
+    void upgradeTowerMe()
+    {
+        CoinBalance coinBalance = CoinBalance.Instance;
+    }
+    
 }
