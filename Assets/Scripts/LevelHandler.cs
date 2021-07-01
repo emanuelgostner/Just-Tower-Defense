f05ba8ca711644a17a4f10f3d28e5bce8831108d
David Deket <david.deket@stud.fh-campuswien.ac.at>
2021-07-01 19:58:39 +0200

    additional yarak

---
+++ Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,64 @@
+public static class LevelHandler
+{
+    private static int _userDifficultySelection = 3;
+    private static int _monstersToBeSpawned = 3;
+    private static int _currentMonsters = 0;
+    private static int _monstersToBeSpawnedAtMost = 5;
+    private static int _currentRound = 1;
+    private static int _timeForMonsterToPassToGoal = 6;
+    private static int _destroyedMonsters = 0;
+
+    // Handles spawning of monsters and start of new round
+    public static int GetMonstersToBeSpawned()
+    {
+        return _monstersToBeSpawned;
+    }
+    
+    public static int GetCurrentMonsters()
+    {
+        return _currentMonsters;
+    }
+    
+    public static void DecreaseMonstersToBeSpawnedAndIncreaseCurrentMonsters()
+    {
+        _monstersToBeSpawned--;
+        _currentMonsters++;
+    }
+
+    public static void DecreaseCurrentMonsters()
+    {
+        _currentMonsters--;
+    }
+    
+    // Handles balance of game
+    public static void SetMonstersToBeSpawned(int howManyMonstersEachRound)
+    {
+        _userDifficultySelection = howManyMonstersEachRound;
+        _monstersToBeSpawned = _userDifficultySelection;
+    }
+
+    public static int GetCoinsPerFinishedRound(int currentRound)
+    {
+        if (currentRound != 0)
+        {
+            return (_monstersToBeSpawnedAtMost * (_currentRound + 2) - GetCoinsPerFinishedRound(_currentRound - 1)) /
+                   _timeForMonsterToPassToGoal;
+        }
+        return 0;
+    }
+
+    // Handle rounds
+    public static void StartNewRound()
+    {
+        CoinBalance.Instance.AddToCoinBalance(GetCoinsPerFinishedRound(_currentRound) * _destroyedMonsters / _userDifficultySelection);
+        _destroyedMonsters = 0;
+        _currentRound++;
+        // TODO: maybe a delay between rounds
+        _monstersToBeSpawned = _userDifficultySelection;
+    }
+
+    public static void IncreaseDestroyedMonsters()
+    {
+        _destroyedMonsters++;
+    }
+}
---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    private float nextSpawnTime;
+
+    [SerializeField] private GameObject cubeMonster;
+    [SerializeField] private float spawnDelay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(cubeMonster, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        if (Time.time > nextSpawnTime && LevelHandler.GetMonstersToBeSpawned() > 0)
+        {
+            LevelHandler.DecreaseMonstersToBeSpawnedAndIncreaseCurrentMonsters();
+            return true;
+        }
+        return false;
+    }
+}
